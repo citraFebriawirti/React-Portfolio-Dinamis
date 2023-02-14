867831ee44b14261081337dc96b4f833f7b56eb1
citraFebriawirti <febriawirticitra@gmail.com>
2023-02-14 09:44:30 +0700

    Citra || card project

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -16,12 +16,12 @@ export default function Project() {
           <hr className="w-40 h-1 my-2 bg-teal-600 border-0 rounded  absolute px-2 -translate-x-1/2 left-1/2 dark:bg-gray-700" />
           <div className="py-4 mx-32 grid grid-cols-1  lg:grid-cols-2 ">
           {projek.map((item) => (
-            <div className="shadow-lg  mb-10 group container rounded-md bg-teal-300  w-48 flex justify-center items-center mx-auto content-div lg:w-96" data-aos="zoom-in" data-aos-duration="1000" key={item.id}>
+            <div className="shadow-lg  mb-10 group container rounded-md bg-white  max-w-sm flex justify-center items-center mx-auto content-div" data-aos="zoom-in" data-aos-duration="1000" key={item.id}>
               <div>
-                <div className="py-8 px-7 bg-white rounded-b-md fd-cl group-hover:opacity-25 lg:py-8 lg:px-4">
+                <div className="py-8 px-2 bg-white rounded-b-md fd-cl group-hover:opacity-25 lg:py-8 lg:px-4">
                   <img src={"../image/" + item.image_projek} />
-                  <span className="block text-lg text-gray-800 font-bold tracking-wide">{item.span1_projek}</span>
-                  <span className="block text-gray-600 text-sm">{item.span2_projek}</span>
+                  <span className="block  text-gray-800 font-bold tracking-wide text-sm text-justify lg:text-lg">{item.span1_projek}</span>
+                  <span className="block text-gray-600 text-[10px] text-justify lg:text-sm">{item.span2_projek}</span>
                 </div>
               </div>
               <div className="absolute opacity-0 fd-sh group-hover:opacity-100">
